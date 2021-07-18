01b6c20e2aa8f11452fcce5b81bae90937cbfb0d
raptor72 <mctaras51@yandex.ru>
2021-07-18 12:19:06 +0400

    prepare to migrate on postgres

--- haskerengine/settings.py
+++ haskerengine/settings.py
@@ -98,6 +98,14 @@ DATABASES = {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
+    #     'NAME': 'hasker',
+    #     'USER': 'hasker',
+    #     'PASSWORD': 'qwe123',
+    #     'HOST': '127.0.0.1',
+    #     'PORT': '5432',
+    # }
 }
 
 
